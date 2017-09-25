cb6331fe248111f9110d61b32d19adedb82e162a
Dimitrios Tampakis <dimitamp@auth.gr>
2017-09-25 16:47:02 +0300

    extension final

--- js/rangeslider.js
+++ js/rangeslider.js
@@ -7,10 +7,18 @@ $(function() {
       animate: true,
       value: 1,
       min: 1,
-      max: 100,
+      max: 13,
       step: 1,
       slide: function(event, ui) {
-        $projectPercent.html(ui.value + "%");
+        if(ui.value==1){
+          $projectPercent.html(ui.value+" month of browsing history");
+        }
+        else if (ui.value==13){
+          $projectPercent.html("All browsing history");
+        }
+        else{
+        $projectPercent.html(ui.value+" month/s of browsing history");
+      }
       },
 
     });
